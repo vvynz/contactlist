1d08b2dd6e9e3537d5c1d287296efc7e59c09834
Vivian Zhang <vvynz12@gmail.com>
2022-08-04 19:49:04 -0400

    Create a formData state to capture input data from the form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React, { useState } from "react";
+
 import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    address: "",
+    number: "",
+    email: ""
+  });
+
   return (
     <div className="app-container">
       <table>
@@ -23,10 +32,30 @@ function App() {
       </table>
       <h2>Add New Contact</h2>
       <form>
-        <input type="text" name="fullName" required="required" placeholder="Enter full name..." />
-        <input type="text" name="address" required="required" placeholder="Enter address..." />
-        <input type="text" name="number" required="required" placeholder="Enter phone number..." />
-        <input type="email" name="email" required="required" placeholder="Enter email..." />
+        <input
+          type="text"
+          name="fullName"
+          required="required"
+          placeholder="Enter full name..."
+        />
+        <input
+          type="text"
+          name="address"
+          required="required"
+          placeholder="Enter address..."
+        />
+        <input
+          type="text"
+          name="number"
+          required="required"
+          placeholder="Enter phone number..."
+        />
+        <input
+          type="email"
+          name="email"
+          required="required"
+          placeholder="Enter email..."
+        />
         <button type="submit">Add</button>
       </form>
     </div>
