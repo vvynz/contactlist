56fb22932c0970b8024ed03899dbacc41e228f25
Vivian Zhang <vvynz12@gmail.com>
2022-08-04 19:30:24 -0400

    Set up table with some static data and some styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Kim Seokjin</td>
+            <td>123 Gangnam St</td>
+            <td>10-613-0709</td>
+            <td>jinhit@hybe.com</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
